98563fff4db196369a357fa510f9fe570c2fca06
Visal Perera <xtrzne@gmail.com>
2022-02-14 13:35:23 -0500

    fixed - titles of tv shows werent showing up

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,6 @@ import Thumbnail from "./Thumbnail";
 import FlipMove from "react-flip-move";
 
 function Results({ results }) {
-  console.log({ results });
   return (
     <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:flex flex-wrap justify-center">
       {results &&
